f38c2553c2a0c35054cefdb66b8b0a99d971c41d
RobinJoby <robinjoby2004@gmail.com>
2025-03-09 17:26:28 +0530

    route change

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/home',
+      initialRoute: '/',
       routes: {
         '/': (context) => const LoginScreen(),
         '/register': (context) => RegisterScreen(),
